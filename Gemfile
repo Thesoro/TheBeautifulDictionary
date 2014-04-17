febdb529a5b8b5214498a81d7403a32ffb2af828
Martin Nash <martin.j.nash@gmail.com>
2014-04-16 21:19:01 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.4'
+
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'figaro'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'pg'
+gem 'pry'
+gem 'sass-rails', '~> 4.0.2'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+
+group :test do
+  gem 'minitest-rails-capybara'
+  gem 'minitest-rails'
+  gem 'turn'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeautifulDictionary::Application.config.secret_key_base = 'bfebc725cc3cc48f06278ae82062ef5f374f207ba772e683915134dbba00db6e8d429966b1f50e2c73627b71283bb8374594ec3ee127b4c3d5d84cb950c354d1'
