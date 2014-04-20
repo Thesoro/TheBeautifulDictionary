65e5fc1d5b2830a28aa688d93b46817d45bc9bf1
Martin Nash <martin.j.nash@gmail.com>
2014-04-19 18:38:08 -0700

    Changed Definition.content to be type text. We definitely want to allow more than 255 characters in a definition.

--- db/migrate/20140419195959_create_definitions.rb
+++ db/migrate/20140419195959_create_definitions.rb
@@ -2,7 +2,7 @@ class CreateDefinitions < ActiveRecord::Migration
   def change
     create_table :definitions do |t|
       t.references :word, index: true
-      t.string :content
+      t.text :content
       t.string :part_of_speech
 
       t.timestamps
