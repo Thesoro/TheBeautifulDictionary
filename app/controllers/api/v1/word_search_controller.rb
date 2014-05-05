5654a253fd8a949525e7be6a190f37528d131784
Martin Nash <martin.j.nash@gmail.com>
2014-05-05 03:31:28 -0700

    Made word search API better
     - case insensitive search
     - return error upon receipt of blank spelling
     - added tests for case insensitive searching

--- app/controllers/api/v1/word_search_controller.rb
+++ app/controllers/api/v1/word_search_controller.rb
@@ -1,26 +1,32 @@
 class Api::V1::WordSearchController < ApplicationController
 
-  before_action :find_spelling
+  before_action :check_blank
 
   def starting_with
-    @words = Word.where("spelling like '#{@spelling}%'")
+    @words = Word.where("lower(spelling) like '#{spelling.downcase}%'")
     render json: { words: @words }
   end
 
   def ending_with
-    @words = Word.where("spelling like '%#{@spelling}'")
+    @words = Word.where("lower(spelling) like '%#{spelling.downcase}'")
     render json: { words: @words }
   end
 
   def containing
-    @words = Word.where("spelling like '%#{@spelling}%'")
+    @words = Word.where("lower(spelling) like '%#{spelling.downcase}%'")
     render json: { words: @words }
   end
 
 
   private
 
-  def find_spelling
-    @spelling = params[:spelling]
+  def spelling
+    @spelling ||= params[:spelling]
+  end
+
+  def check_blank
+    if spelling.blank?
+      render json: { message: "No blank searches allowed." }, status: :bad_request
+    end
   end
 end
--- test/api/search_words_api_test.rb
+++ test/api/search_words_api_test.rb
@@ -3,37 +3,82 @@ require 'test_helper'
 class SearchWordTest < ActionDispatch::IntegrationTest
   
   describe 'GET /api/v1/words_starting_with' do
-    it 'should return a list of words beginning with "home"' do
+
+    it 'errors for emtpy query' do
+      get '/api/v1/words/starting_with'
+      status.must_equal 400
+    end
+
+    it 'returns words beginning with "home"' do
       get '/api/v1/words/starting_with', { spelling: 'home' }
       status.must_equal 200
     
       spellings = returned_spellings
       spellings.count.must_equal 3
-      all_words_must_match spellings, /^home\w*/
+      all_words_must_match spellings, /^home\w*/i
+    end
+
+    it 'returns a case insensitive words beginning with "HOME"' do
+      get '/api/v1/words/starting_with', { spelling: 'HOME' }
+      status.must_equal 200
+    
+      spellings = returned_spellings
+      spellings.count.must_equal 3
+      all_words_must_match spellings, /^home\w*/i
     end
   end
 
 
-  describe 'GET /api/v1/words/ending_with' do
-    it 'should return a list of words ending with "ity"' do
+  describe 'GET /api/v1/words/ending_with {spelling: "__WORD__"}' do
+
+    it 'errors for emtpy query' do
+      get '/api/v1/words/starting_with'
+      status.must_equal 400
+    end
+
+    it 'finds words ending with "ity"' do
       get '/api/v1/words/ending_with', { spelling: 'ity' }
       status.must_equal 200
     
       spellings = returned_spellings
       spellings.count.must_equal 3
-      all_words_must_match spellings, /\w*ity$/
+      all_words_must_match spellings, /\w*ity$/i
+    end
+
+    it 'finds case insensitive words ending with "ITY"' do
+      get '/api/v1/words/ending_with', { spelling: 'ITY' }
+      status.must_equal 200
+    
+      spellings = returned_spellings
+      spellings.count.must_equal 3
+      all_words_must_match spellings, /\w*ity$/i
     end
   end
 
 
   describe 'GET /api/v1/words/containing' do
+
+    it 'errors for emtpy query' do
+      get '/api/v1/words/starting_with'
+      status.must_equal 400
+    end
+
     it 'should return a list of words containing "ppe"' do
       get '/api/v1/words/containing', { spelling: 'ppe' }
       status.must_equal 200
 
       spellings = returned_spellings
       spellings.count.must_equal 3
-      all_words_must_match spellings, /\w*ppe\w*$/
+      all_words_must_match spellings, /\w*ppe\w*$/i
+    end
+
+    it 'returns words containing "PPE"' do
+      get '/api/v1/words/containing', { spelling: 'PPE' }
+      status.must_equal 200
+
+      spellings = returned_spellings
+      spellings.count.must_equal 3
+      all_words_must_match spellings, /\w*ppe\w*$/i
     end
   end
 
